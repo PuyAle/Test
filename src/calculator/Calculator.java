0bdd049fd4ac5f5b1e0962b8bf377e1ba22ed72f
root <root@DESKTOP-R94IB3A.localdomain>
2019-02-19 14:23:47 +0100

    initial commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class Calculator {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int sub(int x, int y) {
+        return x - y;
+    }
+
+    public int mult(int x, int y) {
+        return x * y;
+    }
+
+    public int div(int x, int y) {
+        return x / y;
+    }
+
+}
---
+++ src/calculator/run.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class run {
+
+    public static void main(String[] args) {
+    }
+
+}

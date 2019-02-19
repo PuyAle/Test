2dc1f55812a76a392e2097d767889709548f0738
root <root@DESKTOP-R94IB3A.localdomain>
2019-02-19 14:25:00 +0100

    Commiting test

---
+++ test/calculator/CalculatorTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import java.lang.AssertionError;
+
+/**
+ *
+ * @author puyaa
+ */
+public class CalculatorTest {
+
+    Calculator c;
+
+    @Before
+    public void setUp() {
+        c = new Calculator();
+    }
+
+    @Test
+    public void testAdd() {
+
+        int result = c.add(1, 1);
+        assertEquals(2, result);
+
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testFailAdd() {
+        int result = c.add(1, 1);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testSub() {
+        int result = c.sub(2, 1);
+        assertEquals(1, result);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testFailSub() {
+        int result = c.sub(2, 1);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testMult() {
+        int result = c.mult(2, 1);
+        assertEquals(2, result);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testMultiFail() {
+
+        int result = c.mult(2, 1);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testDiv() {
+        int result = c.div(2, 1);
+        assertEquals(2, result);
+    }
+
+    @Test//(expected = AssertionError.class)
+    public void testFailDiv() {
+        int result = c.div(2, 1);
+        //assertEquals(1, result);
+        //assertNotEquals(1, result);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivZero() {
+        int result = c.div(3, 0);
+        // assertEquals(0, result);
+    }
+
+}
